a8ea193c8e94233d449d69d814719839e91414cb
JLans <lansford.jl@gmail.com>
2016-01-12 21:08:40 -0500

    added anode transfer coefficient

--- anode_H2_oxidation.m
+++ anode_H2_oxidation.m
@@ -1,8 +1,7 @@
-%Wang Journal of The Electrochemical Society, 153 (9) A1732-A1740 (2006)
+
 N = 400; %max overpotential
 
-%Tafel eta = a + b*log(i)
-%Butler-Volmer j = jo(e^(-alph*n*f*eta)-e^((1-alpha)*n*f*eta)
+%Wang Journal of The Electrochemical Society, 153 (9) A1732-A1740 (2006)
 jo = 2; %mA/cm^2 exchange flux at equilibrium
 b = 30; %mV/decade parameter from rotating disk electrode (T?, alpha?)
 eta = 0:N; %mV overpotential
@@ -17,6 +16,28 @@ plot(eta,jK,eta,jVB);
 xlabel('overpotential (mV)');
 ylabel('current density (mA/cm^2)');
 legend('VB-kinetic current','VB-current')
+title('Reproducing Results from Wang')
 ylim([0 1200])
 
+%Butler-Volmer with alpha = 0.48
+T = 274; %K
+jo = 2; %mA/cm^2 exchange flux at equilibrium
+n = 2;
+alpha = .48; %anodic transfer coefficient
+R = 8.314; %J/molK
+F = 96485; %A*s/mol
+eta = 0:N; %mV overpotential
+mVV = 10^3; %mV/V
+jL = 1000; %mV/cm^2 mass transport limiting current
+jK = jo*(exp(alpha*F/(R*T)/mVV*eta)-exp(-alpha*F/(R*T)/mVV*eta)); %kinetic current density
+jf = jo*(exp(alpha*F/(R*T)/mVV*eta)); %kinetic current of forward reaction
+jVB = jK./(1+jf/jL);
+
+figure(2)
+plot(eta,jK,eta,jVB);
+xlabel('overpotential (mV)');
+ylabel('current density (mA/cm^2)');
+legend('VB-kinetic current','VB-current')
+title('Data from Gasteiger')
+ylim([0 1200])
 
