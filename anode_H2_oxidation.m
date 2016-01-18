ae8153e329b8670edcd1d776e026b0162511ab50
JLans <lansford.jl@gmail.com>
2016-01-18 00:47:31 -0500

    Used two electrons and added Volmer Ea

--- anode_H2_oxidation.m
+++ anode_H2_oxidation.m
@@ -1,11 +1,13 @@
 N = 1; %max overpotential
 kB = 8.61733*10^-5; %ev/K
 e = 1.60218 *10^-19; % Coloumbs
-T = 298; %K
+T = 293; %K
 n = 2;
 R = 8.314; %J/molK
 F = 96485; %A*s/mol
 eta = 0:0.001:N; %V overpotential
+EaV = 0.15; %eV Volmer Activation Energy
+jL = 100; %A/cm^2 mass transport limiting current
 %Butler-Volmer with alpha = 0.48
 %{
 %Norskov 2005
@@ -15,48 +17,41 @@ K = @(delGh,T) -delGh/(kB*T);
 k0 = 200; % 1/(s*sites)
 %}
 
-%Skulason_2007
+%Skulason_2007 (DFT numbers)
 SA = 6.64*10^-16; %cm^2/atom
-v = 10^13; %1/(s*sites); prefactor
+v = 7.55*10^12; %10^13; %1/(s*sites); prefactor -this value is sketchy
+%Tafel Mechanism
 EaT = 0.8; %Tafel Reaction eV
-koT = v*exp(-EaT/(kB*T));
-joT = koT*e/SA;
-alphaT = .35; %anodic transfer coefficient
-
-EaH = 0.6; %Heyrovsky Reaction eV
-koH = v*exp(-EaH/(kB*T));
-joH = koH*e/SA;
-alphaH = .52; %anodic transfer coefficient
-
-
-jL = 100; %A/cm^2 mass transport limiting current
-jKT = jo*(exp(2*alphaT*F/(R*T)*eta)-exp(-2*(1-alphaT)*F/(R*T)*eta)); %kinetic current density
-jfT = jo*(exp(2*alphaT*F/(R*T)*eta)); %kinetic current of forward reaction
+koT = v*exp(-(EaT+EaV)/(kB*T));
+joT = koT*2*e/SA;
+alphaT = .36; %anodic transfer coefficient
+jKT = joT*(exp(2*alphaT*F/(R*T)*eta)-exp(-2*(1-alphaT)*F/(R*T)*eta)); %kinetic current density
+jfT = joT*(exp(2*alphaT*F/(R*T)*eta)); %kinetic current of forward reaction
 jVBT = jKT./(1+jfT/jL);
-
-jKH = jo*(exp(2*alphaH*F/(R*T)*eta)-exp(-2*(1-alphaH)*F/(R*T)*eta)); %kinetic current density
-jfH = jo*(exp(2*alphaH*F/(R*T)*eta)); %kinetic current of forward reaction
+%Heyrovsky Mechanism
+EaH = 0.6; % Reaction eV
+koH = v*exp(-(EaH+EaV)/(kB*T));
+joH = koH*2*e/SA;
+alphaH = .52; %anodic transfer coefficient
+jKH = joH*(exp(2*alphaH*F/(R*T)*eta)-exp(-2*(1-alphaH)*F/(R*T)*eta)); %kinetic current density
+jfH = joH*(exp(2*alphaH*F/(R*T)*eta)); %kinetic current of forward reaction
 jVBH = jKH./(1+jfH/jL);
-
-%combined Heyrovsky and Volmer
-jK = jKT+jKH;
-jf = jfT+jfH;
-
-jVB = jK./(1+jf/jL);
 
 %Rheinlander 2014 (Experimental)
 alphaE = 0.48;
 joE = 0.55*10^-3;
-jKE = jo*(exp(2*alphaE*F/(R*T)*eta)-exp(-2*(1-alphaE)*F/(R*T)*eta)); %kinetic current density
-jfE = jo*(exp(2*alphaE*F/(R*T)*eta)); %kinetic current of forward reaction
+jKE = joE*(exp(2*alphaE*F/(R*T)*eta)-exp(-2*(1-alphaE)*F/(R*T)*eta)); %kinetic current density
+jfE = joE*(exp(2*alphaE*F/(R*T)*eta)); %kinetic current of forward reaction
 jVBE = jKE./(1+jfE/jL);
 
 
+
+
 figure(3)
-plot(eta,jVB,eta,jVBE,eta,jVBT,eta,jVBH);
+plot(eta,jVBE,eta,jVBT,eta,jVBH);
 xlabel('Overpotential (V)');
 ylabel('Current density (A/cm^2)');
-legend('DFT-current','Experimental-current','DFT-Tafel','DFT-Heyrovsky')
+legend('Experimental Fit','DFT-Tafel','DFT-Heyrovsky')
 title('Current Density with limiting current of 100 A')
 ylim([0 125])
-xlim([0 0.7])
+xlim([0 1])
